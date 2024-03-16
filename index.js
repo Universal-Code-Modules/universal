9f0c5789999acf2edc9f6e4b7936fbdb941e567b
Leon Polak <lp0808@gmail.com>
2024-03-16 19:38:39 +0200

    Email translations

--- index.js
+++
@@ -1,9 +0,0 @@
-const tr = require('./lib/EMAIL/translations');
-console.log(tr('en', 'passwordResetSubject', {domain: 'domain.com'}))
-console.log(tr('ru', 'passwordResetSubject', {domain: 'domain.com'}))
-console.log(tr('en', 'passwordResetContent', {username: 'John', link: 'https://domain.com', domain: 'domain.com'}))
-console.log(tr('ru', 'passwordResetContent', {username: 'John', link: 'https://domain.com', domain: 'domain.com'}))
-console.log(tr('en', 'emailConfirmationSubject', {domain: 'domain.com'}))
-console.log(tr('ru', 'emailConfirmationSubject', {domain: 'domain.com'}))
-console.log(tr('en', 'emailConfirmationContent', {username: 'John', link: 'https://domain.com', domain: 'domain.com'}))
-console.log(tr('ru', 'emailConfirmationContent', {username: 'John', link: 'https://domain.com', domain: 'domain.com'}))
